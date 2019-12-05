e9a7ba6d1533e70810e798652471291305f43643
Bill Burcham <bburcham@pivotal.io>
2019-12-04 17:13:08 -0800

    scheduler/test scheduler first bones

---
+++ geode-core/src/integrationTest/java/org/apache/geode/distributed/internal/membership/gms/fd/VirtualTimeTest.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.distributed.internal.membership.gms.fd;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VirtualTimeTest {
+
+  private VirtualTime virtualTime;
+  private TestScheduler scheduler;
+
+  @Before
+  public void before() {
+    virtualTime = new VirtualTime();
+    scheduler = new TestScheduler(virtualTime);
+  }
+
+  @Test
+  public void schedulerHoldsReadyTaskBeforeTriggerActions() {
+
+    final AtomicBoolean flag = new AtomicBoolean(false);
+
+    scheduler.schedule(()->flag.set(true), 0, MILLISECONDS);
+
+    assertThat(flag.get()).isFalse().describedAs("scheduler ran task prematurely");
+  }
+
+  @Test
+  public void triggerActionsRunsReadyTask() {
+
+    final AtomicBoolean flag = new AtomicBoolean(false);
+
+    scheduler.schedule(()->flag.set(true), 0, MILLISECONDS);
+
+    scheduler.triggerActions();
+
+    assertThat(flag.get()).isTrue().describedAs("scheduler failed to run task");
+  }
+
+  @Test
+  public void triggerActionsHoldsFutureTask() {
+
+    final AtomicBoolean flag = new AtomicBoolean(false);
+
+    scheduler.schedule(()->flag.set(true), 10, MILLISECONDS);
+
+    scheduler.triggerActions();
+
+    assertThat(flag.get()).isFalse();
+  }
+
+  @Test
+  public void recursiveTaskScheduling() {
+
+    final AtomicBoolean flag = new AtomicBoolean(false);
+
+    scheduler.schedule(()->{
+      scheduler.schedule(()->flag.set(true), 0, MILLISECONDS);
+    }, 0, MILLISECONDS);
+
+    scheduler.triggerActions();
+
+    assertThat(flag.get()).isTrue();
+  }
+
+  @Test
+  public void recursiveTaskSchedulingTwoSteps() {
+
+    final AtomicBoolean flag = new AtomicBoolean(false);
+
+    scheduler.schedule(()->{
+      scheduler.schedule(()->flag.set(true), 1, MILLISECONDS);
+    }, 0, MILLISECONDS);
+
+    scheduler.triggerActions();
+
+    assertThat(flag.get()).isFalse().describedAs("triggerActions ran task prematurely");
+
+    virtualTime.advance(1, MILLISECONDS);
+
+    scheduler.triggerActions();
+
+    assertThat(flag.get()).isTrue().describedAs("triggerActions failed to run ready task");
+  }
+}
